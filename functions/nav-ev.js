bfa962457b845fadb103fcec5d44ade964b8c500
yaseressa <yaseressa222@gmail.com>
2022-09-27 21:47:48 +0300

    This is a new commit for what I originally planned to be amended

---
+++ functions/nav-ev.js
@@ -0,0 +1,40 @@
+document.getElementsByClassName('pl')[1].addEventListener('mouseover', () =>{
+    document.getElementsByClassName('ab-men')[0].classList.add('dview')
+    
+})
+
+document.getElementsByClassName('pl')[1].addEventListener('mouseout', () =>{
+    document.getElementsByClassName('ab-men')[0].classList.remove('dview')
+    
+
+})
+
+document.getElementsByClassName('pl')[2].addEventListener('mouseover', () =>{
+    document.getElementsByClassName('ac-men')[0].classList.add('dview')
+    
+
+})
+
+document.getElementsByClassName('pl')[2].addEventListener('mouseout', () =>{
+    document.getElementsByClassName('ac-men')[0].classList.remove('dview')
+
+
+})
+document.getElementsByClassName('pl')[3].addEventListener('mouseover', () =>{
+    document.getElementsByClassName('re-men')[0].classList.add('dview')
+
+})
+
+document.getElementsByClassName('pl')[3].addEventListener('mouseout', () =>{
+    document.getElementsByClassName('re-men')[0].classList.remove('dview')
+
+})
+document.getElementsByClassName('pl')[4].addEventListener('mouseover', () =>{
+    document.getElementsByClassName('ad-men')[0].classList.add('dview')
+
+})
+
+document.getElementsByClassName('pl')[4].addEventListener('mouseout', () =>{
+    document.getElementsByClassName('ad-men')[0].classList.remove('dview')
+
+})
